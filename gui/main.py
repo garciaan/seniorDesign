321eea524a8bb80e7418c0caad1d76f9a644cf4a
Andy Garcia <andy.w.garcia@gmail.com>
2017-02-17 13:56:51 -0800

    Added sliders and stop button. Note sliders do not currently send anything. Need to stretch them across a couple rows.

--- gui/main.py
+++ gui/main.py
@@ -5,8 +5,9 @@
 
 import sys, os
 from PyQt5.QtWidgets import (QWidget, QGridLayout, 
-    QPushButton, QApplication, QCheckBox)
+    QPushButton, QApplication, QCheckBox, QSlider)
 from PyQt5 import (QtGui,QtCore)
+
 import serial
 sys.path.append(os.path.dirname(__file__) + "../XboxController/")
 from XboxController import XboxController
@@ -44,18 +45,26 @@ class MainWindow(QWidget):
         self.setLayout(grid)
     
         delay = 50
-        interval = 10
+        interval = 5
+
+        stop_button = QPushButton(text = "Stop")
+        stop_button.setAutoRepeat(1)
+        stop_button.setAutoRepeatDelay(delay)
+        stop_button.pressed.connect(self.stop)
+
         forward_button = QPushButton(text = "Forward")
         forward_button.setAutoRepeat(1)
         forward_button.setAutoRepeatDelay(delay)
         forward_button.setAutoRepeatInterval(interval)
         forward_button.pressed.connect(self.forward)
 
+        
         reverse_button = QPushButton(text = "Reverse")
         reverse_button.setAutoRepeat(1)
         reverse_button.setAutoRepeatDelay(delay)
         reverse_button.setAutoRepeatInterval(interval)
         reverse_button.pressed.connect(self.reverse)
+
 
         right_button = QPushButton(text = "Right")
         right_button.setAutoRepeat(1)
@@ -81,35 +90,49 @@ class MainWindow(QWidget):
         sr_button.setAutoRepeatInterval(interval)
         sr_button.pressed.connect(self.swing_right)
 
-        bt_button = QPushButton(text = "Connect to Tank")
-        bt_button.pressed.connect(self.bt_connect)
-
-        bt_close_button = QPushButton(text = "Disconnect Tank")
-        bt_close_button.pressed.connect(lambda: self.bt_close(self.ser))
+        bt_button = QCheckBox(text = "Connect to Drone")
+        bt_button.pressed.connect(lambda: self.bt_handle(bt_button))
 
         xbox_button = QCheckBox(text = "Use Xbox Controller")
         xbox_button.setChecked(False)
         xbox_button.stateChanged.connect(lambda:self.setXboxSend(xbox_button))
 
+        self.sliders = []
+        for x in range (3):
+            self.sliders.append(QSlider(QtCore.Qt.Vertical))
+            self.sliders[x].setMinimum(0)
+            self.sliders[x].setMaximum(100)
+            self.sliders[x].setValue(50)
+            self.sliders[x].valueChanged.connect(lambda: self.setSliderValues(x))
+
+        
+
+
         grid.addWidget(forward_button,1,3)
+        grid.addWidget(stop_button,2,3)
         grid.addWidget(right_button,2,4)
         grid.addWidget(left_button,2,2)
         grid.addWidget(reverse_button,3,3)
         grid.addWidget(sl_button,1,2)
         grid.addWidget(sr_button,1,4)
         grid.addWidget(bt_button,1,1)
-        grid.addWidget(bt_close_button,3,1)
-        grid.addWidget(xbox_button,4,1)
+        grid.addWidget(xbox_button,2,1)
+        grid.addWidget(self.sliders[0],1,5)
+        grid.addWidget(self.sliders[1],1,6)
+        grid.addWidget(self.sliders[2],1,7)
 
         
         self.move(300, 150)
         self.setWindowTitle('AquaDrone Controller')
-        self.setWindowIcon(QtGui.QIcon('tank.png'))
         self.show()
 
 
+    def setSliderValues(self,motor):
+        self.motor_powers[motor] = self.sliders[motor].value()
          
-
+    def defaultStop(self,button):
+        if (button.isChecked()):
+            pass
 
     def printMotorPower(self):
         if (self.left_motor_power < 50 and self.right_motor_power < 50):
@@ -121,22 +144,34 @@ class MainWindow(QWidget):
 
    
 
-    def bt_connect(self,port = '/dev/cu.HC-05-DevB'):
-        try:
-            self.ser = serial.Serial(port)
-            self.bluetooth_connected = True
-            print ("Connected on " + self.ser.name)
-        except ValueError:
-            self.ser = None
-            print ("Could not connect to " + port + ": Value Error")
+    def bt_handle(self,button,port = '/dev/cu.HC-05-DevB'):
+        if button.isChecked() == True:
+            try:
+                self.ser = serial.Serial(port)
+                self.bluetooth_connected = True
+                print ("Connected on " + self.ser.name)
+            except ValueError:
+                self.ser = None
+                print ("Could not connect to " + port + ": Value Error")
 
-        except serial.SerialException:
-            self.ser = None
-            print ("Could not connect to " + port + ": Device not found")
-        
-        except:
-            self.ser = None
-            print ("Could not connect to " + port + ":Unknown error")
+            except serial.SerialException:
+                self.ser = None
+                print ("Could not connect to " + port + ": Device not found")
+            
+            except:
+                self.ser = None
+                print ("Could not connect to " + port + ":Unknown error")
+        else:
+            try:
+                if (self.ser.is_open):
+                    print ("Closing " + port.name)
+                    port.close()
+                    self.bluetooth_connected = False
+                else:
+                    print (port.name + " is not open")
+            except Exception as e:
+                print ("Invalid Port")
+                print (e)
 
     def bt_close(self,port):
         try:
@@ -152,6 +187,8 @@ class MainWindow(QWidget):
     def swing_right(self):
         try:
             print("Swing Right")
+            self.sliders[0].setValue(100)
+            self.sliders[1].setValue(50)
             self.ser.write(bytes([100,50,0,126]))
         except:
             print ("Could not send swing right command")
@@ -159,13 +196,26 @@ class MainWindow(QWidget):
     def swing_left(self):
         try:
             print("Swing Left")
+            self.sliders[0].setValue(50)
+            self.sliders[1].setValue(100)
             self.ser.write(bytes([50,100,0,126]))
         except:
             print ("Could not send swing left command")
 
+    def stop(self):
+        try:
+            print ("Stop")
+            self.sliders[0].setValue(50)
+            self.sliders[1].setValue(50)
+            self.ser.write(bytes([50,50,0,126]))
+        except:
+            print ("Could not send stop command")
+
     def forward(self):
         try:
             print ("Forward")
+            self.sliders[0].setValue(100)
+            self.sliders[1].setValue(100)
             self.ser.write(bytes([100,100,0,126]))
         except:
             print ("Could not send forward command")
@@ -173,6 +223,8 @@ class MainWindow(QWidget):
     def reverse(self):
         try:
             print ("Reverse")
+            self.sliders[0].setValue(0)
+            self.sliders[1].setValue(0)
             self.ser.write(bytes([0,0,0,126]))
         except:
             print ("Could not send reverse command")
@@ -180,6 +232,8 @@ class MainWindow(QWidget):
     def right(self):
         try:
             print ("Right")
+            self.sliders[0].setValue(100)
+            self.sliders[1].setValue(0)
             self.ser.write(bytes([100,0,0,126]))
         except:
             print ("Could not send right command")
@@ -187,6 +241,8 @@ class MainWindow(QWidget):
     def left(self):
         try:
             print ("Left")
+            self.sliders[0].setValue(0)
+            self.sliders[1].setValue(100)
             self.ser.write(bytes([0,100,0,126]))
         except:
             print ("Could not send left command")

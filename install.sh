fce509ad920c3dfef568b003711a87027dc8139a
Andy Garcia <andy.w.garcia@gmail.com>
2017-03-19 15:37:34 -0700

    Updated the install file

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
-sudo easy_install pip
-sudo pip install pyserial
+#!/bin/bash
+if [ `uname` == 'Darwin' ]
+then
+	echo "MacOS Detected"
+	sudo easy_install pip
+	sudo pip install pyserial
+
+elif [ `uname` == 'Linux' ]
+then
+	echo "Linux Detected"
+	sudo apt-get update
+	sudo apt-get upgrade
+	sudo apt-get install python3-serial -y
+	sudo apt-get install python3-pip -y
+	sudo pip3 install --upgrade pip
+	sudo pip3 install pygame
+else
+	echo "Unkown Operating System"
+fi
